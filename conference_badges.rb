3202c2d7b97c16ea0766aadd19dc62ab5c3427d6
Tom Holmes <tholmes59@gmail.com>
2019-01-16 19:33:43 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -22,10 +22,10 @@ def assign_rooms(speakers)
 end
 
 def printer(attendees)
-  batch_badge_creator(name).each do |names|
+  batch_badge_creator(attendees.each do |names|
     puts names
   end
-  assign_rooms(speakers).each do |room|
+  assign_rooms(attendees).each do |room|
     puts room
   end
 end
